d2df98d0ea137f527b76d99c458a528953304ace
Duncan Campbell <duncan.campbell@yale.edu>
2018-10-09 15:42:58 -0400

    added data directory

---
+++ data/download_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# download checksums
+wget https://data.sdss.org/sas/dr10/boss/lss/lss-dr10.sha1sum
+
+# CMASS
+#wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_North-photoObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_North-specObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_North.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_South-photoObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_South-specObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_CMASS_South.fits.gz
+
+# LOWZ
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_North-photoObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_North-specObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_North.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_South-photoObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_South-specObj.fits.gz
+wget https://data.sdss.org/sas/dr10/boss/lss/galaxy_DR10v8_LOWZ_South.fits.gz
+
+
+
+
