ffd6dca3345b9232e5fe567b9473fbd9538e9c3e
Duncan Campbell <duncan@donut-hole.local>
2018-10-25 15:50:59 +0100

    added missing download

--- data/LRG/download_data.sh
+++ data/LRG/download_data.sh
@@ -2,7 +2,7 @@
 
 # large scale structure catalog
 # http://sdss.physics.nyu.edu/lss/dr72/
-#wget http://sdss.physics.nyu.edu/lss/dr72/lss_index.dr72.fits
+wget http://sdss.physics.nyu.edu/lss/dr72/lss_index.dr72.fits
 
 # object catalogs
 # http://sdss.physics.nyu.edu/vagc-dr7/vagc2
@@ -20,4 +20,7 @@ wget http://sdss.physics.nyu.edu/vagc-dr7/vagc2/object_sdss_imaging.fits
 # http://sdss.physics.nyu.edu/vagc-dr7/vagc2/
 wget http://sdss.physics.nyu.edu/vagc-dr7/vagc2/object_sdss_spectro.fits
 
+# lrg catalog
+# http://cosmo.nyu.edu/~eak306/SDSS-LRG.html
+wget http://cosmo.nyu.edu/~eak306/files/DR7-Full.ascii
 
