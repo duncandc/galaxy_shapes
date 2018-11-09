a147e79dbe5480f7684eff27e8153dad335a903f
Duncan Campbell <duncan@mc-wifi-10-215-132-23.wireless.pitt.edu>
2018-11-09 16:16:03 -0500

    working on extinction models

--- shape_models/extinction_model_components.py
+++ shape_models/extinction_model_components.py
@@ -17,20 +17,34 @@ class PS08DustExtinction(object):
     class to model the inclincation dependent affect of dust extinction on observed magnitude
     """
 
-    def __init__(self, gal_type, band='r', **kwargs):
+    def __init__(self, gal_type='centrals', band='r', morphology='disk', **kwargs):
         r"""
         Parameters
         ----------
+        band : string
+            optical band for extinction model
 
+        morphology : string
+            morphology of galaxies to apply extinction model to
+ 
+        Notes
+        -----
+        When this model is used to populate a simulation, the galaxy table
+        must contain a boolean column with name `morphology` indicating 
+        whether the galaxy has that morphology.
         """
 
         self.gal_type = gal_type
-        self.band = band # optical band for extinction model
 
-        self._mock_generation_calling_sequence = (['assign_morphology'])
+        # optical band for extinction model
+        self.band = band
+        # morphological type
+        self.morphology = morphology
+
+        self._mock_generation_calling_sequence = (['assign_extinction'])
 
         self._galprop_dtypes_to_allocate = np.dtype(
-            [(str('galaxy_deltaMag_'+str(self.band)), 'f4')])
+            [(str('deltaMag_'+str(self.band)), 'f4')])
 
         self.list_of_haloprops_needed = []
 
@@ -38,53 +52,76 @@ class PS08DustExtinction(object):
 
         self.set_params(**kwargs)
 
+
     def set_params(self, **kwargs):
         """
-        Notes
-        -----
-        The beta distributions in this model are parameterized by a mean and variance.
+        see tables 4, 5, 6, and 7 in PS08
         """
         
+        # values of parameter 'E0'
+        table_4 = [0.20, 0.72, 0.80, 0.72, 0.44]
+        table_5 = [0.40, 0.60, 0.70, 1.00]
+        table_6 = [0.60, 0.16, 0.16]
+        table_7 = [1.90, 0.72, 0.59]
+
         if 'sample' in kwargs.keys():
             sample = kwargs['sample']
         else:
+            # set dafult sample
             sample = 'all'
+        
+        try:
+            s1, s2, s3 = sample.split('_')
+        except ValueError:
+            s1 = sample
 
-        if sample=='luminosity_sample_1':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.20})
-        elif sample=='luminosity_sample_2':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.72})
-        elif sample=='luminosity_sample_3':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.8})
-        elif sample=='luminosity_sample_4':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.72})
-        elif sample=='all':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.44})
-        # parameters from table 5 in PS08
-        elif sample=='color_sample_1':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.4})
-        elif sample=='color_sample_2':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.6})
-        elif sample=='color_sample_3':
-            param_dict = ({'E0_' + 'r_' + self.gal_type:0.7})
-        elif sample=='color_sample_4':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 1.0})
-        # parameters from table 6 in PS08
-        elif sample=='size_sample_1a':
-            param_dict = ({'E0_' + 'r_' + self.gal_type:0.6})
-        elif sample=='size_sample_1b':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.16})
-        elif sample=='size_sample_1c':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.16})
-        # parameters from table 7 in PS08
-        elif sample=='size_sample_2a':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 1.9})
-        elif sample=='size_sample_2b':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.72})
-        elif sample=='size_sample_2c':
-            param_dict = ({'E0_' + 'r_' + self.gal_type: 0.59})
+        if sample == 'all':
+            E0 = table_4[4]
+        elif s1 == 'luminosity':
+            if   s3 == '1':
+                E0 = table_4[0]
+            elif s3 == '2':
+                E0 = table_4[1]
+            elif s3 == '3':
+                E0 = table_4[2]
+            elif s3 == '4':
+                E0 = table_4[3]
+            else:
+                msg = ('`sample` not recognized.')
+                raise ValueError(msg)
+        elif s1 == 'color':
+            if   s3 == '1':
+                E0 = table_5[0]
+            elif s3 == '2':
+                E0 = table_5[1]
+            elif s3 == '3':
+                E0 = table_5[2]
+            elif s3 == '4':
+                E0 = table_5[3]
+            else:
+                msg = ('`sample` not recognized.')
+                raise ValueError(msg)
+        elif s1 == 'size':
+            if   s3 == '1a':
+                E0 = table_6[0]
+            elif s3 == '1b':
+                E0 = table_6[1]
+            elif s3 == '1c':
+                E0 = table_6[2]
+            elif s3 == '2a':
+                E0 = table_7[0]
+            elif s3 == '2b':
+                E0 = table_7[1]
+            elif s3 == '2c':
+                E0 = table_7[2]
+            else:
+                msg = ('`sample` not recognized.')
+                raise ValueError(msg)
+        else:
+            msg = ('`sample` not recognized.')
+            raise ValueError(msg)
 
-        self.param_dict = param_dict
+        self.param_dict = {'E0_'+self.band+'_'+self.gal_type : E0}
 
 
     def extinction_model(self, theta, y):
@@ -98,44 +135,62 @@ class PS08DustExtinction(object):
 
         y : array_like
             y = c/b
+
+        Notes
+        -----
+        It is possible PS08 meant for y=c/a.
         """
         
         E0 = self.param_dict['E0_'+self.band+'_'+self.gal_type]
         result = np.zeros(len(y)) + E0
         
-        mask = (np.cos(theta)>y)
+        # for mostly face-on galaxies, set extinction to 0 
+        mask = (np.cos(theta) > y)
+
+        # otherwise apply a non-zero extinction
         e = (1.0 + y - np.cos(theta))*E0
         result[mask] = e[mask]
 
         return result
 
 
-    def assign_deltaMag_r(self, **kwargs):
+    def assign_extinction(self, **kwargs):
         r"""
+        Parameters
+        ----------
+        b_to_a : array_like
+
+        c_to_a : array_like
+
+        theta : array_like
+
+        Returns
+        -------
+        delta_Mag : numpy.array
+            array of extinctions in magnitudes
         """
 
         if 'table' in kwargs.keys():
             table = kwargs['table']
-            N = len(table)
             b_to_a = table['galaxy_b_to_a']
             c_to_a = table['galaxy_c_to_a']
             theta = table['galaxy_theta']
+            N = len(table)
         else:
-            mag = kwargs['mag']
             theta = kwargs['theta']
             b_to_a = kwargs['b_to_a']
             c_to_a = kwargs['c_to_a']
+            N = len(theta)
 
-        #y = c/b
+        # y = c/b
         y = c_to_a/b_to_a
 
-        mask = (table['spiral']==True)
-        result = np.zeros(N)
-
-        result[mask] = self.extinction_model(mag, theta, y)
+        result = self.extinction_model(theta, y)
 
         if 'table' in kwargs.keys():
-            mask = (table['gal_type'] == self.gal_type)
+            mask_1 = (table[self.morphology]==True)
+            mask_2 = (table['gal_type'] == self.gal_type)
+            mask = (mask_1 & mask_2)
             table['galaxy_deltaMag_r'][mask] = result[mask]
         else:
             return result
@@ -146,7 +201,7 @@ class Shao07DustExtinction(object):
     class to model inclincation dependent dust extinction
     """
 
-    def __init__(self, gal_type, **kwargs):
+    def __init__(self, gal_type='centrals', morphology='disk', **kwargs):
         r"""
         Parameters
         ----------
@@ -154,6 +209,7 @@ class Shao07DustExtinction(object):
         """
 
         self.gal_type = gal_type
+        self.morphology = morphology
 
         self._mock_generation_calling_sequence = (['assign_extinction'])
 
@@ -202,18 +258,23 @@ class Shao07DustExtinction(object):
         gamma_i = self.param_dict['gamma_i_' + self.gal_type]
         gamma_z = self.param_dict['gamma_z_' + self.gal_type]
         
+        N = len(theta)
         result = np.zeros((N,5))
-        result[:,0] = gamma_u * np.log10(np.cos(theta))
-        result[:,1] = gamma_g * np.log10(np.cos(theta))
-        result[:,2] = gamma_r * np.log10(np.cos(theta))
-        result[:,3] = gamma_i * np.log10(np.cos(theta))
-        result[:,4] = gamma_z * np.log10(np.cos(theta))
+        result[:,0] = -1.0*gamma_u * np.log10(np.cos(theta))
+        result[:,1] = -1.0*gamma_g * np.log10(np.cos(theta))
+        result[:,2] = -1.0*gamma_r * np.log10(np.cos(theta))
+        result[:,3] = -1.0*gamma_i * np.log10(np.cos(theta))
+        result[:,4] = -1.0*gamma_z * np.log10(np.cos(theta))
 
         return result
 
 
     def assign_extinction(self, **kwargs):
         r"""
+        Parameters
+        ----------
+        theta : array_like
+
         """
 
         if 'table' in kwargs.keys():
@@ -227,7 +288,9 @@ class Shao07DustExtinction(object):
         result = self.extinction_model(theta)
 
         if 'table' in kwargs.keys():
-            mask = (table['gal_type'] == self.gal_type)
+            mask_1 = (table[self.morphology]==True)
+            mask_2 = (table['gal_type'] == self.gal_type)
+            mask = (mask_1 & mask_2)
             table['deltaMag_u'][mask] = result[mask,0]
             table['deltaMag_g'][mask] = result[mask,1]
             table['deltaMag_r'][mask] = result[mask,2]
