1d4713e902641cb843354eefe1b0034c53be7428
Duncan Campbell <duncan@donut-hole.lan>
2018-11-13 15:22:30 -0500

    small changes

--- sdss_analysis/sdss_shape_model.py
+++ sdss_analysis/sdss_shape_model.py
@@ -1,20 +1,25 @@
 """
-model for galaxy shaops
+model for SDSS galaxy shapes
 """
 
+# model for fraction of disk and elliptical galaxies
 from galaxy_shapes.shape_models.morphology_model_components import Morphology_2
 morpholopgy_model = Morphology_2()
 
+# model for 3D disk and elliptical galaxy shapes
 from galaxy_shapes.shape_models.shape_model_components import EllipticalGalaxyShapes, DiskGalaxyShapes
 elliptical_shape_model = EllipticalGalaxyShapes()
 disk_shape_model       = DiskGalaxyShapes()
 
+# model for galaxy alignments
 from intrinsic_alignments.ia_models.ia_model_components import RandomAlignment
 orientation_model = RandomAlignment()
 
+# model for projected galaxy shapes
 from galaxy_shapes.shape_models.shape_model_components import ProjectedShapes
 proj_shapes_model = ProjectedShapes()
 
+# model for disk galaxuy dust extinction
 from galaxy_shapes.shape_models.extinction_model_components import PS08DustExtinction, Shao07DustExtinction
 extinction_model = Shao07DustExtinction()
 
@@ -24,10 +29,11 @@ prof_model = TrivialPhaseSpace()
 from halotools.empirical_models import Zheng07Cens
 occupation_model =  Zheng07Cens()
 
+# combine model componenets
 from halotools.empirical_models import HodModelFactory
 model_instance = HodModelFactory(centrals_occupation_model = occupation_model,
-	                             centrals_prof_model = prof_model,
-	                             centrals_morphology = morpholopgy_model,
+	                              centrals_prof_model = prof_model,
+	                              centrals_morphology = morpholopgy_model,
                                  centrals_elliptical_galaxy_shape = elliptical_shape_model,
                                  centrals_disk_galaxy_shape = disk_shape_model,
                                  centrals_orientation = orientation_model,
