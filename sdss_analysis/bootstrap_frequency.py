4776887601e69115101b0323968213e347ba6a7c
Duncan Campbell <duncan@donut-hole.lan>
2018-11-13 14:05:06 -0500

    cleaning up

--- sdss_analysis/bootstrap_frequency.py
+++ sdss_analysis/bootstrap_frequency.py
@@ -1,16 +1,39 @@
 """
+function used to caclulate the the distribvution of a quantity in bins where
+errors on the relative frequency are calculated using bootstrap resampling.
 """
 
 from __future__ import print_function, division
 import numpy as np
 
-__all__=['bootstrap_frequency']
+__all__=['bootstrap_frequency', ]
 
 def bootstrap_frequency(x, bins, weights=None, Nboot=100):
     """
-    Measure of the relative frequency of x in bins.
-    Errors are calculated using bootstrap sampling.
+    Measure of the relative frequency of `x` in bins where
+    errors are calculated using bootstrap sampling.
+
+    Parameters
+    -----------
+    x : array_like
+       array of values
+
+    bins : array_like
+        array of bins
+
+    weights : array_like, optional
+       array of floats to use for weights counts
+
+    Nboot : int
+       number of bootstrap samples to use
+
+    Returns
+    -------
+    f, err : list
+       list of number arrays containing the relative frequency and error
     """
+
+    x = np.atleast_1d(x)
     N = len(x)
     
     if weights is None:
