597a99d23f17f97050ef9d564701caf46ae74ba8
Duncan Campbell <duncan@vibmPC0AGAEL.localdomain>
2018-11-27 12:31:05 -0500

    added script to run all fits

---
+++ sdss_analysis/model_fitting/run_all_fits.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run using nohup ./run_all_fits.sh & to keep running in background
+
+python fit_sample.py sample_1
+python fit_sample.py sample_2
+python fit_sample.py sample_3
+python fit_sample.py sample_4
+python fit_sample.py sample_5
+python fit_sample.py sample_6
