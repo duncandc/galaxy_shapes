bd90b33916d46281742cb94556d81b9b44898ead
Duncan Campbell <duncan.campbell@yale.edu>
2019-01-17 18:05:41 -0500

    working on size models

--- shape_models/size_model_components.py
+++ shape_models/size_model_components.py
@@ -231,6 +231,7 @@ class Shen03DiskGalaxySizes(object):
     	alpha = self.param_dict['elliptical_size_alpha']
     	beta = self.param_dict['elliptical_size_beta']
     	gamma = self.param_dict['elliptical_size_gamma']
+        m0 = self.param_dict['elliptical_size_m0']
     	return 10.0**(-0.4*alpha*m + (beta-alpha)*np.log10(1.0+10.0**(-0.4*(m-m0)))+gamma)
 
     def scatter_size_model(self, m):
